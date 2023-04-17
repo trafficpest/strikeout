a44efc2894753d6aa95f3fc0b81df5c2087c9049
trafficpest <trafficpestsolutions@gmail.com>
2023-04-17 17:41:02 -0600

    added a simple landing page

--- inc/ui/header.php
+++ inc/ui/header.php
@@ -3,14 +3,17 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
+        crossorigin="anonymous">
 </head>
 
 
 <body class="bg-dark">
 <nav class="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand">
+  <a class="navbar-brand" href="<?=$path_to_root.'/index.php'?>">
   <img src="<?=$path_to_root.'/assets/images/logo.png'?>" alt="StrikeOut" 
         class="rounded" width="140" height="60"></a> 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" i
--- index.php
+++ index.php
@@ -7,5 +7,34 @@ require_once $path_to_root.'/inc/session.php';
 ?>
 
 <?include $path_to_root.'/inc/ui/header.php'?>
+
+<div class="container bg-light shadow rounded mt-5 w-80">
+<h1 class="text-center pt-3">Welcome</h1>
+<p class="text-center">Lets get you started</p>
+<div class="row p-2">
+  <div class="col text-center">
+  <a href="<?=$path_to_root.'/pages/invoice.php'?>">
+  <img src="<?=$path_to_root.'/assets/images/qr.png'?>" 
+        alt="Invoices" 
+        class="rounded img-fluid pb-2"></a>
+  <p>Create Invoices</p>
+  </div>
+  <div class="col text-center">
+  <a href="<?=$path_to_root.'/pages/webhooks.php'?>">
+  <img src="<?=$path_to_root.'/assets/images/webhook-icon.png'?>" 
+        alt="Webhooks" 
+        class="rounded img-fluid pb-2"></a>
+  <p>Manage Webhooks</p>
+  </div>
+  <div class="col text-center">
+  <a href="<?=$path_to_root.'/pages/setup.php'?>">
+  <img src="<?=$path_to_root.'/assets/images/settings.png'?>" 
+        alt="Settings" 
+        class="rounded img-fluid pb-2"></a>
+  <p>Configure</p>
+  </div>
+</div>
+</div>
+
 <?include $path_to_root.'/inc/ui/footer.php'?>
 
